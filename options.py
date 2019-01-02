a1dfcb000b3b32918af881d1a6547daa0eac6c50
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-01-02 16:20:50 +0100

    Update options.py

--- options.py
+++ options.py
@@ -5,7 +5,6 @@ from optparse import *
 parser = OptionParser(add_help_option=False)
 parser.add_option("-e", "--encrypt", dest="encrypt", help="encrypt")
 parser.add_option("-d", "--decrypt", dest="decrypt", help="decrypt")
-parser.add_option("-v", "--validys", dest="validys", help="validys")
 (options, args) = parser.parse_args()
 
 encrypt = options.encrypt
